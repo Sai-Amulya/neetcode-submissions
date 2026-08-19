b984d8fa54337857ab108f145ad70a2340bca7c5
Sai Amulya Yellapragada <s.amulya.yellapragada@gmail.com>
2026-08-18 22:10:16 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.4
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Doctor"
+print(type(variable))
+variable = [4, 4, 8, 3, 1]
+print(type(variable))
