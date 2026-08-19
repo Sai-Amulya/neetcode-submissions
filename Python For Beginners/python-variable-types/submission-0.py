e023c70cad9f1e7a1486c0cd49f37843bb73d315
Sai Amulya Yellapragada <s.amulya.yellapragada@gmail.com>
2026-08-18 22:07:24 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 10
+float_type = 11.1
+bool_type = False
+string_type = "Amazon"
+list_type = ["Amazon", "Google", "Meta", "Apple", "Nvidia", "OpenAI", "Oracle"]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
